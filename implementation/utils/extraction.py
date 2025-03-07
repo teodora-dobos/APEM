4aa70b4bdf8f20eb19e1f470b27b59558e2dbb29
Lukas0912 <lukas_weigmann@web.de>
2025-03-07 22:53:59 +0100

    update extraction file

--- implementation/utils/extraction.py
+++ implementation/utils/extraction.py
@@ -1,8 +1,17 @@
-def extract_from_buyers(df_buyers, column, buyer, period, bid=None):
+from typing import Any
+
+import pandas as pd
+
+
+def extract_from_buyers(df_buyers: pd.DataFrame, column: str, buyer: int, period: int, bid=None):
     return df_buyers[(df_buyers['buyer'] == buyer) & (df_buyers['period'] == period)][
         column + ("" if bid is None else str(bid))].iloc[0]
 
 
-def extract_from_sellers(df_sellers, column, seller, period, bid=None):
+def extract_from_sellers(df_sellers: pd.DataFrame, column: str, seller: int, period: int, bid=None):
     return df_sellers[(df_sellers['seller'] == seller) & (df_sellers['period'] == period)][
         column + ("" if bid is None else str(bid))].iloc[0]
+
+
+def get(df: pd.DataFrame, column: str, order_id: int) -> Any:
+    return df.loc[df['id'] == order_id, column].values[0]
